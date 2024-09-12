a67dac1bd2cce76668a178bb6e4a8cb82936c958
Maksym Mohyla <greenparksquad@gmail.com>
2024-09-12 19:18:50 +0300

    fixed code and deployed page

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,17 +1,14 @@
-const getPartnerText = person => {
-  if (person.isMarried) {
-    return person.sex === 'm'
-      ? `${person.partnerName} is my wife`
-      : `${person.partnerName} is my husband`;
-  }
+export const Person = ({ person }) => {
+  const { name, age, isMarried, sex, partnerName } = person;
+  const partner = sex === 'm' ? 'wife' : 'husband';
 
-  return 'I am not married';
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">
+        {isMarried ? `${partnerName} is my ${partner}` : 'I am not married'}
+      </p>
+    </section>
+  );
 };
-
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age && <p className="Person__age">I am {person.age}</p>}
-    <p className="Person__partner">{getPartnerText(person)}</p>
-  </section>
-);
